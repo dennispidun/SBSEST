e60bce7bd391cace3781613d539b0afb7b9ed796
Dennis Pidun <dennis.pidun@gmail.com>
2020-05-10 17:51:40 +0200

    give it a try...

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off 
+clean.bat
 REM ACHTUNG: Diese müssen zwingend mit einer Kommandozeile aus dem Projektordner heraus aufgerufen werden! Es wird keinerlei Haftung für die Auswirkungen dieses Skripts übernommen.
 REM Der Name der Haupt-Tex-Datei (z.B. Arbeit) muss als Argument übergeben werden.
 cd %~dp0
@@ -11,4 +12,3 @@ pdflatex  -synctex=1 -interaction=nonstopmode --src-specials "%texSource%.tex"
 pdflatex  -synctex=1 -interaction=nonstopmode --src-specials "%texSource%.tex"
 makeindex "%texSource%.nlo" -s nomencl.ist -o "%texSource%.nls"
 pdflatex  -synctex=1 -interaction=nonstopmode --src-specials "%texSource%.tex"
-clean.bat
