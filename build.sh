c15d787166b0f7c0448e8fce6c70a5ff673a4139
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-05-11 02:02:48 +0200

    add linux control scripts

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pdflatex  -synctex=1 -interaction=nonstopmode --src-specials "Arbeit.tex"
+bibtex "Arbeit"
+makeindex "Arbeit.nlo" -s nomencl.ist -o "Arbeit.nls"
+makeglossaries "Arbeit"
+pdflatex  -synctex=1 -interaction=nonstopmode --src-specials "Arbeit.tex"
+pdflatex  -synctex=1 -interaction=nonstopmode --src-specials "Arbeit.tex"
+makeindex "Arbeit.nlo" -s nomencl.ist -o "Arbeit.nls"
+pdflatex  -synctex=1 -interaction=nonstopmode --src-specials "Arbeit.tex"
